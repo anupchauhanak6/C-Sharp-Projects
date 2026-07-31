7f2862a4f288c696d66579ae372274642ba03519
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-07-31 10:22:21 +0530

    Refactor Program.cs to define a Program class and update the greeting message

--- ATM-Simulation-System/Program.cs
+++ ATM-Simulation-System/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Hello!");
+    }
+}
